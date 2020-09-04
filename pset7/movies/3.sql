827117b9e6782353e130965b881122206e40a078
Muhammad Najib <muhammad@Muhammads-MacBook-Air.local>
2020-09-04 17:15:51 -0400

    5.sql done

---
+++ pset7/movies/3.sql
@@ -0,0 +1,5 @@
+--a release date on or after 2018, in alphabetical order.
+ select  title from movies
+ where year >= 2018
+ ORDER BY title ASC;
+
---
+++ pset7/movies/5.sql
@@ -0,0 +1,3 @@
+select title,year from movies
+where title LIKE 'HARRY POTTER%'
+ORDER BY year ASC;
